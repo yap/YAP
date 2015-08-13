958e47c17ee77c772a45e14cc9299875d446ab76
Daniel Greenwald <daniel.greenwald@tum.de>
2015-08-13 19:44:21 +0200

    [programs/D3piTest.cxx] Tests DataSet::addDataSet

--- programs/D3piTest.cxx
+++ programs/D3piTest.cxx
@@ -10,9 +10,19 @@ int main( int argc, char** argv)
 {
 
     std::vector<TLorentzVector> P;
-    P.push_back(TLorentzVector(0,1,2,3));
-    P.push_back(TLorentzVector(1,2,3,0));
-    P.push_back(TLorentzVector(2,3,0,1));
-    P.push_back(TLorentzVector(3,0,1,2));
+    P.push_back(TLorentzVector(0, 1, 2, 3));
+    P.push_back(TLorentzVector(1, 2, 3, 0));
+    P.push_back(TLorentzVector(2, 3, 0, 1));
+    P.push_back(TLorentzVector(3, 0, 1, 2));
+
+    yap::DataSet DS;
+    DS.addDataPoint(yap::DataPoint(P)); // move
+
+    yap::DataPoint D(P);
+    DS.addDataPoint(D);         // copy and move
+    DS.addDataPoint(std::move(D)); // move
+
+    P.pop_back();
+    DS.addDataPoint(yap::DataPoint(P)); // fail
 
 }
