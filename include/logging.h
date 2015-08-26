de57381359a025084974aab7b01c077dcf32299b
Johannes Rauch <j.rauch@tum.de>
2015-08-26 17:51:43 +0200

    add description how to use logging

--- include/logging.h
+++ include/logging.h
@@ -3,9 +3,28 @@
 
 #include "easylogging++.h"
 
-/// \todo Add instructions on how to use the log
+/**
+ * \file logging.h
+ * \brief Logging system using easylogging++
+ *
+ * Usage:
+ * you have to include this header and call the following macro line in your program:
+ * \code
+ * #include "logging.h"
+ * INITIALIZE_EASYLOGGINGPP
+ * \endcode
+ *
+ * To create a log message, put the following line into the code:
+ * \code
+ * LOG(level) << "put your log message here";
+ * \endcode
+ *
+ * Available levels:
+ * INFO, WARNING, DEBUG, ERROR, FATAL, TRACE, VERBOSE
+ *
+ * A complete manual for easylogging++ is available at:
+ * https://github.com/easylogging/easyloggingpp/blob/master/README.md
+ */
 
-/// you have to put the following line in your program
-// INITIALIZE_EASYLOGGINGPP
 
 #endif
