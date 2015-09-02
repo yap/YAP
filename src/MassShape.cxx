c051d79cec9946429064cef30e7829b9ac431507
Daniel Greenwald <daniel.greenwald@tum.de>
2015-09-02 18:39:20 +0200

    [MassShape] areEqual : equates by simple content

--- src/MassShape.cxx
+++ src/MassShape.cxx
@@ -2,6 +2,9 @@
 
 #include "Constants.h"
 
+#include <algorithm>
+#include <set>
+
 namespace yap {
 
 //-------------------------
@@ -9,6 +12,21 @@ MassShape::MassShape(unsigned nParameters) :
     DataAccessor(),
     Parameters_(nParameters, 0)
 {
+}
+
+//-------------------------
+bool MassShape::areEqual(std::shared_ptr<ParticleCombination> A, std::shared_ptr<ParticleCombination> B) const
+{
+    // check size of indices vectors
+    if (A->indices().size() != B->indices().size())
+        return false;
+
+    // check contents of indices vectors
+    // (creating a set will sort entries for easy comparison,
+    // since order doesn't matter)
+    std::set<ParticleIndex> a(A->indices().begin(), A->indices().end());
+    std::set<ParticleIndex> b(B->indices().begin(), B->indices().end());
+    return std::equal(a.begin(), a.end(), b.begin());
 }
 
 }
