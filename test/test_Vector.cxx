1abc40d937c0a8b4def0468dcefb54f97e4026a1
Johannes Rauch <j.rauch@tum.de>
2015-12-14 15:29:14 +0100

    extend vector test

--- test/test_Vector.cxx
+++ test/test_Vector.cxx
@@ -15,9 +15,55 @@ TEST_CASE( "Vector" )
     yap::disableLogs(el::Level::Global);
 
     SECTION( "FourVector" ) {
-        // cache J = 0, 1/2, 1 (out of order)
+        // +=
+        {
+            yap::FourVector<double> v({1,2,3,4});
+            v += yap::FourVector<double>({5,6,7,8});
+            REQUIRE( v == yap::FourVector<double>({6,8,10,12}) );
+        }
+
+        // -=
+        {
+            yap::FourVector<double> v({1,2,3,4});
+            v -= yap::FourVector<double>({5,6,7,8});
+            REQUIRE( v == yap::FourVector<double>({-4,-4,-4,-4}) );
+        }
+
+        // *=
+        {
+            yap::FourVector<double> v({1,2,3,4});
+            v *= 2.;
+            REQUIRE( v == yap::FourVector<double>({2,4,6,8}) );
+        }
+
+        // +
+        REQUIRE( yap::FourVector<double>({1,2,3,4}) + yap::FourVector<double>({5,6,7,8}) == yap::FourVector<double>({6,8,10,12}) );
+
+        // -
+        REQUIRE( yap::FourVector<double>({1,2,3,4}) - yap::FourVector<double>({5,6,7,8}) == yap::FourVector<double>({-4,-4,-4,-4}) );
+
+        // *
+        REQUIRE( yap::FourVector<double>({1,2,3,4}) * yap::FourVector<double>({5,6,7,8}) == -60 );
+
+        // norm
+        REQUIRE( norm(yap::FourVector<double>({1,0,0,0})) == 1 );
+        REQUIRE( norm(yap::FourVector<double>({5,6,7,8})) == -124 );
+
+        // abs
         REQUIRE( abs(yap::FourVector<double>({1,0,0,0})) == 1 );
         REQUIRE( abs(yap::FourVector<double>({1,1,0,0})) == 0 );
+
+        // unit
+        //REQUIRE( unit(yap::FourVector<double>({1,0,0,0})) == yap::FourVector<double>({1,0,0,0}) );
+
+        // *
+
+        // -
+
+        // cross
+
+        // angle
+
     }
 
 }
