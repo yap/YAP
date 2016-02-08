ef1bc35ad275affaaf6ae0a769b135d037989a2b
Daniel Greenwald <daniel.greenwald@tum.de>
2016-02-08 17:10:20 +0100

    [examples/bat_gen] updated to fit with last changes in core code

--- examples/bat_gen/dkkpi.cxx
+++ examples/bat_gen/dkkpi.cxx
@@ -35,18 +35,18 @@ dkkpi::dkkpi(std::string name)
     double radialSize = 3.; // [GeV^-1]
 
     // final state particles
-    std::shared_ptr<yap::FinalStateParticle> kPlus  = Factory_.createFinalStateParticle(+321);
-    std::shared_ptr<yap::FinalStateParticle> kMinus = Factory_.createFinalStateParticle(-321);
-    std::shared_ptr<yap::FinalStateParticle> piPlus = Factory_.createFinalStateParticle(+211);
+    auto kPlus  = Factory_.fsp(+321);
+    auto kMinus = Factory_.fsp(-321);
+    auto piPlus = Factory_.fsp(+211);
 
     // initial state particle
-    D_ = Factory_.createInitialStateParticle(Factory_.pdgCode("D+"), radialSize, std::make_unique<yap::ZemachSpinAmplitudeCache>());
-    // D_ = Factory_.createInitialStateParticle(Factory_.pdgCode("D+"), radialSize, std::make_unique<yap::HelicitySpinAmplitudeCache>());
+    D_ = Factory_.isp(Factory_.pdgCode("D+"), radialSize, std::make_unique<yap::ZemachSpinAmplitudeCache>());
+    // D_ = Factory_.isp(Factory_.pdgCode("D+"), radialSize, std::make_unique<yap::HelicitySpinAmplitudeCache>());
     D_->setFinalStateParticles({kPlus, kMinus, piPlus});
 
     // phi
-    // std::shared_ptr<yap::Resonance> phi = std::make_shared<yap::Resonance>(Factory_.quantumNumbers("phi"), 1010.e-3, "phi", radialSize, std::make_unique<yap::BreitWigner>());
-    std::shared_ptr<yap::Resonance> phi = std::make_shared<yap::Resonance>(yap::QuantumNumbers(2, 0), 1310.e-3, "phi", radialSize, std::make_unique<yap::BreitWigner>());
+    // auto phi = std::make_shared<yap::Resonance>(Factory_.quantumNumbers("phi"), 1010.e-3, "phi", radialSize, std::make_shared<yap::BreitWigner>());
+    auto phi = std::make_shared<yap::Resonance>(yap::QuantumNumbers(2, 0), 1310.e-3, "phi", radialSize, std::make_shared<yap::BreitWigner>());
     static_cast<yap::BreitWigner&>(phi->massShape()).width()->setValue(20e-3);
     phi->addChannel({kPlus, kMinus});
     D_->addChannel({phi, piPlus});
@@ -55,7 +55,7 @@ dkkpi::dkkpi(std::string name)
 
     /*
     // X_2
-    std::shared_ptr<yap::Resonance> X_2 = std::make_shared<yap::Resonance>(yap::QuantumNumbers(4, 0), 1.2, "X_2", radialSize, std::make_unique<yap::BreitWigner>());
+    auto X_2 = std::make_shared<yap::Resonance>(yap::QuantumNumbers(4, 0), 1.2, "X_2", radialSize, std::make_shared<yap::BreitWigner>());
     static_cast<yap::BreitWigner&>(X_2->massShape()).width()->setValue(80e-3);
     X_2->addChannel({piPlus, kMinus});
     D_->addChannel({X_2, kPlus});
