4c8ff2dc59de7771b21d773c7920920e203fc66e
Daniel Greenwald <daniel.greenwald@tum.de>
2016-02-15 18:41:42 +0100

    [test/FourMomenta] filled out test

--- test/test_FourMomentaCalculation.cxx
+++ test/test_FourMomentaCalculation.cxx
@@ -1,21 +1,16 @@
 #include <catch.hpp>
 #include <catch_capprox.hpp>
 
-#include <logging.h>
-#include <BreitWigner.h>
 #include <Constants.h>
-#include <Exceptions.h>
 #include <FinalStateParticle.h>
+#include <DecayingParticle.h>
 #include <FourVector.h>
-#include <HelicitySpinAmplitude.h>
 #include <logging.h>
 #include <MassAxes.h>
+#include <make_unique.h>
 #include <Model.h>
-#include <MathUtilities.h>
 #include <ParticleCombination.h>
 #include <ParticleFactory.h>
-#include <Resonance.h>
-#include <WignerD.h>
 #include <ZemachSpinAmplitude.h>
 
 #include <cmath>
@@ -24,67 +19,95 @@ TEST_CASE( "FourMomentaCalculation" )
 {
 
     // disable logs in text
-    yap::disableLogs(el::Level::Global);
-    //yap::plainLogs(el::Level::Global);
+    // yap::disableLogs(el::Level::Global);
 
+    // load particle factory
+    yap::ParticleFactory factory("../data/evt.pdl");
 
-    // use common radial size for all resonances
-    double radialSize = 3.; // [GeV^-1]
-
-    yap::Model M(std::make_unique<yap::ZemachFormalism>());
-
-    yap::ParticleFactory factory((::getenv("YAPDIR") ? (std::string)::getenv("YAPDIR") : ".") + "/evt.pdl");
-
-    // initial state particle
-    auto D = factory.decayingParticle(factory.pdgCode("D+"), radialSize);
-
-    // final state particles
+    // create final state particles
     auto piPlus = factory.fsp(211);
     auto piMinus = factory.fsp(-211);
 
-    // set final state
+    // create model and set initial and final state
+    yap::Model M(std::make_unique<yap::ZemachFormalism>());
     M.setFinalState({piPlus, piMinus, piPlus});
 
-    // rho
-    auto rho = factory.resonance(113, radialSize, std::make_shared<yap::BreitWigner>());
-    rho->addChannel({piPlus, piMinus});
+    // create book-keeping resonance
+    /// \todo Allow direct phase-space three-body decay
+    auto X = factory.decayingParticle(factory.pdgCode("f_0"), 3);
+    X->addChannel({piPlus, piMinus});
 
-    // Add channels to D
-    D->addChannel({rho,      piPlus});
+    // create initial state particle
+    auto D = factory.decayingParticle(factory.pdgCode("D+"), 3);
+    D->addChannel({X, piPlus});
 
-    M.initializeForMonteCarloGeneration(1);
-    REQUIRE( M.consistent() );
+    // REQUIRE( M.consistent() );
 
     // choose Dalitz coordinates m^2_12 and m^2_23
     const yap::MassAxes massAxes = M.getMassAxes({{0, 1}, {1, 2}});
 
-    std::vector<double> m2 = {0.9, 1.1};
+    auto m_0_range = M.getMassRange(massAxes[0]);
+    auto m_1_range = M.getMassRange(massAxes[1]);
 
-    // calculate final state momenta
-    auto P = M.calculateFourMomenta(massAxes, m2);
+    const unsigned N = 20;
+    for (double m_0 = m_0_range[0]; m_0 <= m_0_range[1]; m_0 += (m_0_range[1] - m_0_range[0]) / N) {
+        for (double m_1 = m_1_range[0]; m_1 <= m_1_range[1]; m_1 += (m_1_range[1] - m_1_range[0]) / N) {
 
+            std::vector<double> m2 = {m_0 * m_0, m_1 * m_1};
 
-    //
-    // require invariant masses to be correct
-    //
-    // fsp
-    for (auto& v : P)
-        REQUIRE( abs(v) == Approx(piPlus->mass()->value()) );
-    // isp
-    yap::FourVector<double> isp = std::accumulate(P.begin(), P.end(), yap::FourVector_0);
-    REQUIRE( abs(isp) == Approx(D->mass()->value()) );
-    // 2-p
-    unsigned i_a(0);
-    for (auto& axis : massAxes) {
-        auto sum = yap::FourVector_0;
-        for (unsigned i : axis->indices())
-            sum +=  P[i];
+            // calculate final state momenta
+            auto P = M.calculateFourMomenta(massAxes, m2);
 
-        REQUIRE( abs(sum) == Approx(m2[i_a]) );
-        ++i_a;
+            //-------------------------
+            // check phase space
+            double m_isp = M.initialStateParticle()->mass()->value();
+            // find a, b, and c such that mass axes are (ab) (bc)
+            double m_a = 0;
+            double m_b = 0;
+            double m_c = 0;
+            for (unsigned i = 0; i < M.finalStateParticles().size(); ++i) {
+                if (std::find(massAxes[0]->indices().begin(), massAxes[0]->indices().end(), i) != massAxes[0]->indices().end()) {
+                    if (std::find(massAxes[1]->indices().begin(), massAxes[1]->indices().end(), i) == massAxes[0]->indices().end())
+                        m_a = M.finalStateParticles()[i]->mass()->value();
+                    else
+                        m_b = M.finalStateParticles()[i]->mass()->value();
+                } else
+                    m_c = M.finalStateParticles()[i]->mass()->value();
+            }
+            // check constraints on m2[0]
+            bool inPhaseSpace = m2[0] >= pow(m_a + m_b, 2) and m2[0] <= pow(m_isp - m_c, 2);
+            // check constraints on m2[1]
+            if (inPhaseSpace) {
+                double Eb = (m2[0] - m_a * m_a + m_b * m_b) / 2. / sqrt(m2[0]);
+                double Ec = (m_isp * m_isp - m2[0] - m_c * m_c) / 2. / sqrt(m2[0]);
+                double Pb = sqrt(Eb * Eb - m_b * m_b);
+                double Pc = sqrt(Ec * Ec - m_c * m_c);
+                inPhaseSpace = fabs(m2[1] - m_b * m_b - m_c * m_c - 2. * Eb * Ec) <= 2. * Pb * Pc;
+            }
+
+            // require P is empty if outside phase space
+            REQUIRE( P.empty() == !inPhaseSpace );
+
+            if (!P.empty()) {
+
+                //-------------------------
+                // check fsp masses
+                for (size_t i = 0; i < P.size(); ++i)
+                    REQUIRE( abs(P[i]) == Approx(M.finalStateParticles()[i]->mass()->value()) );
+
+                //-------------------------
+                // check isp mass
+                // isp
+                auto p_isp = std::accumulate(P.begin(), P.end(), yap::FourVector_0);
+                REQUIRE( abs(p_isp) == Approx(m_isp) );
+
+                // check Dalitz axes
+                for (size_t i = 0; i < massAxes.size(); ++i) {
+                    auto p = std::accumulate(massAxes[i]->indices().begin(), massAxes[i]->indices().end(), yap::FourVector_0, [&](const yap::FourVector<double>& p, unsigned j) {return p + P[j];});
+                    REQUIRE( norm(p) == Approx(m2[i]) );
+                }
+
+            }
+        }
     }
-
-
-    // Require ISP to be in its rest frame
-    REQUIRE( abs(vect(isp)) == Approx(0) );
 }
