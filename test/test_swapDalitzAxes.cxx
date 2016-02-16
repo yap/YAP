76eb618f4a365f39e72a33b40534467df5454f7b
Johannes Rauch <j.rauch@tum.de>
2016-02-16 13:52:33 +0100

    refine test

--- test/test_swapDalitzAxes.cxx
+++ test/test_swapDalitzAxes.cxx
@@ -13,6 +13,10 @@
 
 #include <cmath>
 
+/**
+ * Test that the amplitude remains the same after swapping the axes of the DalitzPlot
+ */
+
 TEST_CASE( "swapDalitzAxes" )
 {
 
@@ -20,78 +24,116 @@ TEST_CASE( "swapDalitzAxes" )
     yap::disableLogs(el::Level::Global);
     //yap::plainLogs(el::Level::Global);
 
+    std::array<double, 4> PDGs = {411, 321, -321, 211}; // D+ -> K+ K- pi+
+    std::array<double, 2> m2_ab_range = {0.4, 1.9};
+    std::array<double, 2> m2_bc_range = {0.9, 3.1};
 
-    std::array<double, 2> m_0_range = {0.4, 1.9};
-    std::array<double, 2> m_1_range = {0.9, 3.1};
+    auto F = yap::ParticleFactory((std::string)::getenv("YAPDIR") + "/data/evt.pdl");
+
 
     const unsigned N = 20;
-    for (double m_0 = m_0_range[0]; m_0 <= m_0_range[1]; m_0 += (m_0_range[1] - m_0_range[0]) / N) {
-        for (double m_1 = m_1_range[0]; m_1 <= m_1_range[1]; m_1 += (m_1_range[1] - m_1_range[0]) / N) {
+    for (double m2_ab = m2_ab_range[0]; m2_ab <= m2_ab_range[1]; m2_ab += (m2_ab_range[1] - m2_ab_range[0]) / N) {
+        for (double m2_bc = m2_bc_range[0]; m2_bc <= m2_bc_range[1]; m2_bc += (m2_bc_range[1] - m2_bc_range[0]) / N) {
 
-            std::vector<std::vector<double> > parameters{{m_0, m_1}, {m_1, m_0}};
+            // calc 3rd inv mass square
+            double m2_ac = pow(F.decayingParticle(PDGs[0], 3.)->mass()->value(), 2)
+                                    + pow(F.fsp(PDGs[1])->mass()->value(), 2)
+                                    + pow(F.fsp(PDGs[2])->mass()->value(), 2)
+                                    + pow(F.fsp(PDGs[3])->mass()->value(), 2)
+                                    - m2_ab - m2_bc;
 
-            std::vector<double> resultingAmplitudes({0, 0});
+            if (m2_ac < 0.){
+                //std::cout << "m2_ac < 0.\n";
+                continue;
+            }
+
+
+            // loop over SpinFormalisms
             for (unsigned i_formalism = 0; i_formalism < 2; ++i_formalism) {
 
-                for (unsigned i = 0; i < 2; ++i) {
+                std::vector<double> resultingAmplitudes({0, 0, 0, 0});
 
-                    auto F = yap::ParticleFactory((std::string)::getenv("YAPDIR") + "/data/evt.pdl");
+                // loop over axis swaps
+                for (unsigned i = 0; i < 4; ++i) {
 
                     // final state particles
-                    auto kPlus  = F.fsp(+321);
-                    auto kMinus = F.fsp(-321);
-                    auto piPlus = F.fsp(+211);
+                    auto kPlus  = F.fsp(PDGs[1]);
+                    auto kMinus = F.fsp(PDGs[2]);
+                    auto piPlus = F.fsp(PDGs[3]);
 
                     auto M = i_formalism == 0 ? std::make_unique<yap::Model>(std::make_unique<yap::ZemachFormalism>()) :
                              std::make_unique<yap::Model>(std::make_unique<yap::HelicityFormalism>());
 
+                    /// \todo have to thest this!!
                     //M->setFinalState({kPlus, kMinus, piPlus});
                     M->setFinalState({piPlus, kMinus, kPlus});
 
-                    // use common radial size for all resonances
-                    double radialSize = 3.; // [GeV^-1]
-
                     // initial state particle
-                    auto D = F.decayingParticle(F.pdgCode("D+"), radialSize);
+                    auto D = F.decayingParticle(PDGs[0], 3.);
 
-                    auto piK0 = yap::Resonance::create(yap::QuantumNumbers(0, 0), 0.75, "piK0", radialSize, std::make_shared<yap::BreitWigner>(0.75, 0.025));
+                    auto piK0 = yap::Resonance::create(yap::QuantumNumbers(0, 0), 0.75, "piK0", 3., std::make_shared<yap::BreitWigner>(0.75, 0.025));
                     piK0->addChannel({piPlus, kMinus});
                     D->addChannel({piK0, kPlus})->freeAmplitudes()[0]->setValue(0.5 * yap::Complex_1);
 
-                    auto piK1 = yap::Resonance::create(yap::QuantumNumbers(2, 0), 1.00, "piK1", radialSize, std::make_shared<yap::BreitWigner>(1.00, 0.025));
+                    auto piK1 = yap::Resonance::create(yap::QuantumNumbers(2, 0), 1.00, "piK1", 3., std::make_shared<yap::BreitWigner>(1.00, 0.025));
                     piK1->addChannel({piPlus, kMinus});
                     D->addChannel({piK1, kPlus})->freeAmplitudes()[0]->setValue(1. * yap::Complex_1);
 
-                    auto piK2 = yap::Resonance::create(yap::QuantumNumbers(4, 0), 1.25, "piK2", radialSize, std::make_shared<yap::BreitWigner>(1.25, 0.025));
+                    auto piK2 = yap::Resonance::create(yap::QuantumNumbers(4, 0), 1.25, "piK2", 3., std::make_shared<yap::BreitWigner>(1.25, 0.025));
                     piK2->addChannel({piPlus, kMinus});
                     D->addChannel({piK2, kPlus})->freeAmplitudes()[0]->setValue(30. * yap::Complex_1);
 
                     M->initializeForMonteCarloGeneration(1);
 
-                    // choose Dalitz coordinates m^2_12 and m^2_23
+
+                    // Dalitz coordinates
                     yap::MassAxes massAxes;
-                    if (i == 0)
+                    std::vector<double> squared_masses;
+                    switch(i) {
+                    case 0:
+                        // original
                         massAxes = M->getMassAxes({{0, 1}, {1, 2}});
-                    else
+                        squared_masses = {m2_ab, m2_bc};
+                        break;
+                    case 1:
+                        // 0 <-> 1
+                        massAxes = M->getMassAxes({{1, 0}, {0, 2}});
+                        squared_masses = {m2_ab, m2_ac};
+                        break;
+                    case 2:
+                        // 0 <-> 2
                         massAxes = M->getMassAxes({{2, 1}, {1, 0}});
+                        squared_masses = {m2_bc, m2_ab};
+                        break;
+                    case 3:
+                        // 1 <-> 2
+                        massAxes = M->getMassAxes({{0, 2}, {2, 1}});
+                        squared_masses = {m2_ac, m2_bc};
+                    }
 
                     // calculate four-momenta
-                    auto P = M->calculateFourMomenta(massAxes, parameters[i]);
+                    auto P = M->calculateFourMomenta(massAxes, squared_masses);
 
                     // if failed, outside phase space
-                    if (P.empty())
+                    if (P.empty()) {
+                        //std::cout<<"PhSp\n";
                         continue;
+                    }
 
                     M->setFinalStateMomenta(M->dataSet()[0], P, 0);
 
                     resultingAmplitudes[i] = M->logOfSquaredAmplitude(M->dataSet()[0], 0);
+                    //std::cout<<resultingAmplitudes[i]<<"   ";
                 }
-            }
 
-            REQUIRE( std::isfinite(resultingAmplitudes[0]) );
-            REQUIRE( resultingAmplitudes[0] == resultingAmplitudes[1] );
+                REQUIRE( std::isfinite(resultingAmplitudes[0]) );
+                REQUIRE( resultingAmplitudes[0] == Approx(resultingAmplitudes[1]) );
+                REQUIRE( resultingAmplitudes[0] == Approx(resultingAmplitudes[2]) );
+                REQUIRE( resultingAmplitudes[0] == Approx(resultingAmplitudes[3]) );
 
+                //std::cout<<"\n";
 
+            }// end loop over SpinFormalisms
 
         }
     }
