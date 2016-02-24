18c837a79ab5c7e78b5d720a2ff5ba1f94f984c3
Johannes Rauch <j.rauch@tum.de>
2016-02-24 15:31:16 +0100

    improve the test

--- test/test_HelicityAngles.cxx
+++ test/test_HelicityAngles.cxx
@@ -20,10 +20,65 @@
 #include <ZemachFormalism.h>
 
 #include <TGenPhaseSpace.h>
+#include <TLorentzRotation.h>
 #include <TLorentzVector.h>
 #include <TRandom.h>
 
 #include <cmath>
+
+/*
+ * Test the calculation of helicity angles
+ */
+
+// copied from rootPWA
+TLorentzRotation hfTransform(const TLorentzVector& daughterLv)
+{
+    TLorentzVector daughter = daughterLv;
+    const TVector3 zAxisParent(0, 0, 1);  // take z-axis as defined in parent frame
+    const TVector3 yHfAxis = zAxisParent.Cross(daughter.Vect());  // y-axis of helicity frame
+    // rotate so that yHfAxis becomes parallel to y-axis and zHfAxis ends up in (x, z)-plane
+    TRotation rot1;
+    rot1.RotateZ(0.5*yap::pi<double>() - yHfAxis.Phi());
+    rot1.RotateX(yHfAxis.Theta() - yap::pi<double>());
+    daughter *= rot1;
+    // rotate about yHfAxis so that daughter momentum is along z-axis
+    TRotation rot2;
+    rot2.RotateY(-yap::signum(daughter.X()) * daughter.Theta());
+    daughter *= rot2;
+    // boost to daughter RF
+    rot1.Transform(rot2);
+    TLorentzRotation hfTransform(rot1);
+    hfTransform.Boost(-daughter.BoostVector());
+    return hfTransform;
+}
+
+void transformDaughters(const yap::Model& M,
+        std::map<const std::shared_ptr<yap::ParticleCombination>, std::vector<double> >& results,
+        const std::shared_ptr<yap::ParticleCombination>& pc,
+        std::vector<TLorentzVector> finalStatesHf)
+{
+    // loop over daughters
+    for (const auto& daugh : pc->daughters()) {
+
+        if (daugh->daughters().empty())
+            continue;
+
+        // construct 4-vector of daughter
+        TLorentzVector daughter;
+        for (unsigned i : daugh->indices())
+            daughter += finalStatesHf.at(i);
+
+        results[daugh] = {daughter.Phi(), daughter.Theta()};
+
+        // next helicity frame
+        const TLorentzRotation transDaugh = hfTransform(daughter);
+        for (unsigned i : daugh->indices())
+            finalStatesHf.at(i).Transform(transDaugh);
+
+        transformDaughters(M, results, daugh, finalStatesHf);
+    }
+}
+
 
 TEST_CASE( "HelicityAngles" )
 {
@@ -72,40 +127,38 @@ TEST_CASE( "HelicityAngles" )
                                     piPlus->mass()->value()
                                   };
 
-    for (unsigned int iEvt = 0; iEvt < 10; ++iEvt) {
+    for (unsigned int iEvt = 0; iEvt < 1; ++iEvt) {
         TGenPhaseSpace event;
         event.SetDecay(P, nParticles, masses);
         event.Generate();
 
         std::vector<yap::FourVector<double> > momenta;
+        std::vector<TLorentzVector> rootMomenta;
+
         for (unsigned i = 0; i < nParticles; ++i) {
             TLorentzVector p = *event.GetDecay(i);
+            rootMomenta.push_back(p);
             momenta.push_back(yap::FourVector<double>({p.T(), p.X(), p.Y(), p.Z()}));
         }
 
         M.addDataPoint(momenta);
         auto dp = M.dataSet().back();
 
-
-        // \todo we are now assuming that the D's coordinate system is (1,0,0; 0,1,0; 0,0,1)
-        // this is ok since it is the default and we didn't change it
-        // actually we would have to rotate
+        // calculate helicity angles with alternative method copied from rootPWA
+        std::map<const std::shared_ptr<yap::ParticleCombination>, std::vector<double> > results;
 
         for (auto pc : D->particleCombinations()) {
             REQUIRE( M.fourMomenta()->m(dp, pc) == Approx(D->mass()->value()) );
 
-            TLorentzVector daughter;
-            for (unsigned i : pc->daughters()[0]->indices()) {
-                daughter += *event.GetDecay(i);
-            }
-
-            DEBUG( to_string(*pc) << "; phi = " << daughter.Phi() << "; theta = " << daughter.Theta());
-
-            REQUIRE( M.helicityAngles()->phi(dp, pc) == Approx(daughter.Phi()) );
-            REQUIRE( M.helicityAngles()->theta(dp, pc) == Approx(daughter.Theta()) );
-
-            // \todo recurse down
+            transformDaughters(M, results, pc, rootMomenta);
         }
+
+        // compare results
+        for (auto& kv : results) {
+            REQUIRE( M.helicityAngles()->phi(dp, kv.first) == Approx(results.at(kv.first)[0]) );
+            REQUIRE( M.helicityAngles()->theta(dp, kv.first) == Approx(results.at(kv.first)[1]) );
+        }
+
 
     }
 }
