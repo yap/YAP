5976fc68f9cdec85b9bf98e93bc3c8b6aef4b28b
Johannes Rauch <j.rauch@tum.de>
2016-03-21 14:01:14 +0100

    clean up

--- test/test_HelicityAngles_boostRotate.cxx
+++ test/test_HelicityAngles_boostRotate.cxx
@@ -26,7 +26,8 @@
 
 /*
  * Test the calculation of helicity angles
- * Downstream theta angles must be the same regardless of rotations/boosts of the final state particles
+ * Downstream theta angles must be the same regardless of rotations/boosts of the final state particles.
+ * Phi can change, since it only affects the phase of the amplitude, and it will change in the same way for all amplitudes
  */
 
 TEST_CASE( "HelicityAngles_boostRotate" )
@@ -73,7 +74,6 @@ TEST_CASE( "HelicityAngles_boostRotate" )
     TLorentzVector P(0., 0., 0., D->mass()->value());
     std::vector<double> masses = { piPlus->mass()->value(), piMinus->mass()->value(), piPlus->mass()->value() };
 
-
     for (unsigned int iEvt = 0; iEvt < 1000; ++iEvt) {
         std::map<std::shared_ptr<yap::ParticleCombination>, std::vector<double>> resultingThetas;
 
@@ -93,34 +93,26 @@ TEST_CASE( "HelicityAngles_boostRotate" )
                 TLorentzVector p = *event.GetDecay(i);
 
                 // testing. Theta of downstream helicity angles must stay the same
-                // Phi can change, since it only affects the phase of the amplitude, and it will change in the same way for all amplitudes
-
                 switch (iTrans) {
                 case 0:
                 default:
                     break;
                 case 1:
-                    //std::cout << "rotate around X by " << angle << "\n";
                     p.RotateX(angle);
                     break;
                 case 2:
-                    //std::cout << "rotate around Y by " << angle << "\n";
                     p.RotateY(angle);
                     break;
                 case 3:
-                    //std::cout << "rotate around Z by " << angle << "\n";
                     p.RotateZ(angle);
                     break;
                 case 4:
-                    //std::cout << "boost along X by " << boost << "\n";
                     p.Boost(boost, 0., 0.);
                     break;
                 case 5:
-                    //std::cout << "boost along Y by " << boost << "\n";
                     p.Boost(0., boost, 0.);
                     break;
                 case 6:
-                    //std::cout << "boost along Z by " << boost << "\n";
                     p.Boost(0., 0., boost);
                     break;
                 }
@@ -132,45 +124,18 @@ TEST_CASE( "HelicityAngles_boostRotate" )
             data.add(momenta);
             const auto dp = data.points().back();
 
-            // check 4 momenta in their rest frames
-            /*for (auto& pc : M.helicityAngles()->particleCombinations()) {
-                auto root_momenta_copy(root_momenta);
-                TLorentzVector b;
-                for (unsigned i : pc->indices())
-                    b += root_momenta_copy[i];
-
-                std::cout << "4momenta in " << to_string(*pc) << " rest frame:\n";
-                for (auto& v : root_momenta_copy) {
-                    v.Boost(-b.BoostVector());
-                    v.Print();
-                }
-
-            }*/
-
             // compare results
-            //std::cout << "transformation " << iTrans << "\n";
-            for (auto& pc : M.helicityAngles()->particleCombinations()) {
-                //std::cout << std::left << std::setw(50) << yap::to_string(*pc);
-                //std::cout << "  helicityAngles():  (" <<  M.helicityAngles()->phi(dp, pc) << ", " << M.helicityAngles()->theta(dp, pc) << ")\n";
-
+            for (auto& pc : M.helicityAngles()->particleCombinations())
                 if (pc->indices().size() < M.finalStateParticles().size())
                     resultingThetas[pc].push_back(M.helicityAngles()->theta(dp, pc));
-            }
-
-            //std::cout << "\n";
         }
-
 
         // check if thetas are equal
         // Phi can change, since it only affects the phase of the amplitude, and it will change in the same way for all amplitudes
         for (auto& kv : resultingThetas) {
             double theta_0 = kv.second[0];
-            for (auto theta : kv.second) {
-                //std::cout << to_string(*kv.first) << ": " << theta << "\n";
+            for (auto theta : kv.second)
                 REQUIRE(theta == Approx(theta_0));
-            }
         }
-
-        //std::cout << "-------------------------------------------------------\n";
     }
 }
