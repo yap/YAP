be45636ce7bdfeeadba70d11562f5df1b8650198
Johannes Rauch <j.rauch@tum.de>
2016-03-24 19:22:49 +0100

    fix bug in HelicityFormalism. We still have to find the correct angular momentum norm factor

--- src/HelicityFormalism.cxx
+++ src/HelicityFormalism.cxx
@@ -12,13 +12,17 @@ namespace yap {
 HelicitySpinAmplitude::HelicitySpinAmplitude(unsigned two_J, unsigned two_j1, unsigned two_j2, unsigned l, unsigned two_s, ParticleCombination::Equiv* equiv) :
     SpinAmplitude(two_J, two_j1, two_j2, l, two_s, equiv)
 {
+    // angular momentum normalization factor
+    /// \todo check which is the right one
+    double c = sqrt(2. * L() + 1);
+    //double c = sqrt((2. * L() + 1) / 4. / pi<double>() );
+    //double c  = sqrt((2. * L() + 1) / (initialTwoJ() + 1.));
+
     // cache coefficients
-    double c = sqrt((2. * L() + 1) / 4. / pi<double>());
     for (int two_m1 = -finalTwoJ()[0]; two_m1 <= (int)finalTwoJ()[0]; two_m1 += 2)
         for (int two_m2 = -finalTwoJ()[1]; two_m2 <= (int)finalTwoJ()[1]; two_m2 += 2)
             try {
-
-                double CG = c * ClebschGordan::couple(finalTwoJ()[0], two_m1, finalTwoJ()[1], two_m2, L(), twoS(), initialTwoJ());
+                double CG = ClebschGordan::couple(finalTwoJ()[0], two_m1, finalTwoJ()[1], two_m2, L(), twoS(), initialTwoJ());
 
                 if (CG == 0)
                     continue;
@@ -58,7 +62,7 @@ std::complex<double> HelicitySpinAmplitude::calc(int two_M, int two_m1, int two_
           << " and helicity angles (" << phi << ", " << theta << ")");
 
     return std::conj(DFunction(initialTwoJ(), two_M, two_m1 - two_m2, phi, theta, 0))
-           * Coefficients_.at(two_m1).at(two_m2);
+           * Coefficients_.at(two_m1).at(two_m2);;
 
     /// \todo Take a look at momentum-dependent Clebsch-Gordan
     /// coefficients by J. Friedrich and S.U. Chung implemented in
