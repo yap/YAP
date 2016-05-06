a9f7319849b8a16bad27a04c6d780882452cc315
pdigiglio <p.digiglio91@gmail.com>
2016-05-06 12:26:42 +0200

    edit scripts to find ROOT and BAT, remove CommonMacros.cmake

--- cmakeModules/FindBAT.cmake
+++ cmakeModules/FindBAT.cmake
@@ -6,7 +6,7 @@ set(BAT_LIBS)
 
 find_program(BAT_CONFIG_EXECUTABLE bat-config)
 if(NOT BAT_CONFIG_EXECUTABLE)
-	set(BAT_ERROR_REASON "${BAT_ERROR_REASON} $Cannot find bat-config executable in path. Make sure BAT is setup correctly.")
+	set(BAT_ERROR_REASON "${BAT_ERROR_REASON} Cannot find bat-config executable in path. Make sure BAT is setup correctly.")
 else()
 
 	set(BAT_FOUND TRUE)
@@ -33,8 +33,11 @@ else()
 	if(NOT EXISTS "${BAT_LIBRARY_DIR}")
 		set(BAT_FOUND FALSE)
 		set(BAT_ERROR_REASON "${BAT_ERROR_REASON} BAT library directory '${BAT_LIBRARY_DIR}' does not exist.")
-		set(BAT_LIBS "${BAT_LIBRARY_DIR} -lBAT")
 	endif()
+
+	execute_process(COMMAND ${BAT_CONFIG_EXECUTABLE} --libs
+		OUTPUT_VARIABLE BAT_LIBS
+		OUTPUT_STRIP_TRAILING_WHITESPACE)
 
 endif()
 
@@ -42,16 +45,16 @@ endif()
 mark_as_advanced(
 	BAT_INCLUDE_DIR
 	BAT_LIBRARY_DIR
-	BAT_LIBRARIES
+	BAT_LIBS
 	)
 
 
 # report result
 if(BAT_FOUND)
-	message(STATUS "Found BAT version ${BAT_VERSION} r${BAT_SVN_REVISION} in '${BATSYS}'.")
+	message(STATUS "Found BAT version ${BAT_VERSION} in '${BATSYS}'.")
 	message(STATUS "Using BAT include directory '${BAT_INCLUDE_DIR}'.")
 	message(STATUS "Using BAT library directory '${BAT_LIBRARY_DIR}'.")
-	message(STATUS "Using BAT libraries ${BAT_LIBRARIES}.")
+	message(STATUS "Using BAT libraries ${BAT_LIBS}.")
 else()
 		message(FATAL_ERROR "Unable to find requested BAT installation:${BAT_ERROR_REASON}")
 endif()
