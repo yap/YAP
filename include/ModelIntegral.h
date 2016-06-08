a74474bf6684918ed15880fe0a92467f77cad365
Daniel Greenwald <daniel.greenwald@tum.de>
2016-06-08 19:56:55 +0200

    [ModelIntegral] doxygen, +access to integral component values (protected)

--- include/ModelIntegral.h
+++ include/ModelIntegral.h
@@ -29,6 +29,18 @@
 
 namespace yap {
 
+/// \class ModelIntegral
+/// \brief Stores integral components for a model
+/// \author Daniel Greenwald
+///
+/// Each DecayTree holds a product of free amplitudes ("a", below)
+/// and a product of data-dependent amplitudes ("A", below)
+/// 
+/// This class holds two types of components:\n
+///   o Diagonal components: one for each DecayTree,
+///     stored as |A|^2 for the tree and returned as |a|^2 * stored value
+///   o Off-diagonal components: one for each pair of DecayTrees (i, j),
+///     stored as conj(A_i) * A_j and returned as 2 * real(conj(a_i) * a_i * stored value)
 class ModelIntegral
 {
 public:
@@ -37,6 +49,7 @@ public:
     /// \param dtv DecayTreeVector to construct integral of
     ModelIntegral(const DecayTreeVector& dtv);
 
+    /// return integral calculated from components
     const double integral() const;
 
     /// \name typedefs
@@ -49,6 +62,19 @@ public:
     using OffDiagonalMap = std::map<std::array<DecayTreeVector::value_type, 2>, std::complex<double> >;
 
     /// @}
+
+protected:
+
+    /// \return value stored in Diagonals_ for particular DecayTree
+    /// \param dt shared_ptr to DecayTree to access diagonal integral component for
+    DiagonalMap::mapped_type& diagonalComponent(const DecayTreeVector::value_type& dt)
+    { return Diagonals_.at(dt); }
+
+    /// \return value stored in OffDiagonals_ for particular DecayTree pair
+    /// \param i shared_ptr to DecayTree to access off-diagonal integral component for
+    /// \param j shared_ptr to DecayTree to access off-diagonal integral component for
+    OffDiagonalMap::mapped_type& offDiagonalComponent(const DecayTreeVector::value_type& i, const DecayTreeVector::value_type& j)
+    { return OffDiagonals_.at(OffDiagonalMap::key_type({i, j})); }
 
 private:
 
