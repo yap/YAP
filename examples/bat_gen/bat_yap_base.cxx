10bc813fe2171109ca2977e87eddb377ac00c85e
Daniel Greenwald <daniel.greenwald@tum.de>
2016-07-04 01:33:58 +0200

    [examples/bat_gen] +bat_yap_base

---
+++ examples/bat_gen/bat_yap_base.cxx
@@ -0,0 +1,30 @@
+// ***************************************************************
+// This file was created using the bat-project script.
+// bat-project is part of Bayesian Analysis Toolkit (BAT).
+// BAT can be downloaded from http://mpp.mpg.de/bat
+// ***************************************************************
+
+#include "bat_yap_base.h"
+
+#include <DecayingParticle.h>
+#include <Exceptions.h>
+#include <Model.h>
+#include <SpinAmplitudeCache.h>
+
+// -----------------------
+bat_yap_base::bat_yap_base(std::string name, std::unique_ptr<yap::Model> M)
+    : BCModel(name),
+      Model_(std::move(M))
+{
+    if (!Model_)
+        throw yap::exceptions::Exception("Model nullptr", "bat_yap_base::bat_yap_base");
+
+    if (!Model_->consistent())
+        throw yap::exceptions::Exception("Model inconsistent", "bat_yap_base::bat_yap_base");
+
+    auto isps = full_final_state_isp(*Model_);
+    if (isps.empty())
+        throw yap::exceptions::Exception("no full-final-state initial-state particle in model", "bat_yap_base::bat_yap_base");
+
+    ISP_ = isps[0];
+}
