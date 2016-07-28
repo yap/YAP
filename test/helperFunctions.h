dc085a9d8668512e2558781fed7bd596bd34589e
Johannes Rauch <j.rauch@tum.de>
2016-07-28 14:39:46 +0200

    new test for integration. Share some code among tests.

---
+++ test/helperFunctions.h
@@ -0,0 +1,76 @@
+#ifndef __test_helper_functions__H
+#define __test_helper_functions__H
+
+#include "BreitWigner.h"
+#include "DataSet.h"
+#include "FinalStateParticle.h"
+#include "HelicityFormalism.h"
+#include "make_unique.h"
+#include "MassAxes.h"
+#include "Model.h"
+#include "Parameter.h"
+#include "ParticleFactory.h"
+#include "PDL.h"
+#include "PHSP.h"
+#include "Resonance.h"
+
+/// generate a model with 4 final state particles
+//-------------------------
+inline std::shared_ptr<yap::Model> create_model()
+{
+    auto M = std::make_shared<yap::Model>(std::make_unique<yap::HelicityFormalism>());
+
+    yap::ParticleFactory factory = yap::read_pdl_file((::getenv("YAPDIR") ? (std::string)::getenv("YAPDIR") + "/data" : ".") + "/evt.pdl");
+    double radialSize = 1.;
+
+    // initial state particle
+    auto D = factory.decayingParticle(421, radialSize);
+
+    // final state particles
+    auto piPlus = factory.fsp(211);
+    auto piMinus = factory.fsp(-211);
+
+    // Set final-state particles
+    M->setFinalState(piPlus, piMinus, piPlus, piMinus);
+
+    // rho
+    auto rho = factory.resonance(113, radialSize, std::make_shared<yap::BreitWigner>());
+    rho->addChannel(piPlus, piMinus);
+
+    // sigma / f_0(500)
+    auto sigma = factory.resonance(9000221, radialSize, std::make_shared<yap::BreitWigner>());
+    sigma->addChannel(piPlus, piMinus);
+
+    // a_1
+    auto a_1 = factory.resonance(20213, radialSize, std::make_shared<yap::BreitWigner>());
+    a_1->addChannel(sigma, piPlus);
+    a_1->addChannel(rho,   piPlus);
+
+    // D's channels
+    D->addChannel(rho, rho);
+    D->addChannel(a_1, piMinus);
+
+    M->addInitialStateParticle(D);
+
+    return M;
+}
+
+//-------------------------
+inline yap::DataSet generate_data(std::shared_ptr<yap::Model> M, unsigned nPoints)
+{
+    auto isp = M->initialStateParticles().begin()->first;
+    auto A = M->massAxes();
+    auto m2r = yap::squared(yap::mass_range(A, isp, M->finalStateParticles()));
+
+    yap::DataSet data(M->createDataSet());
+
+    std::mt19937 g(0);
+    // fill data set with nPoints points
+    std::generate_n(std::back_inserter(data), nPoints,
+                    std::bind(yap::phsp<std::mt19937>, std::cref(*M), isp->mass()->value(), A, m2r, g, std::numeric_limits<unsigned>::max()));
+
+    return data;
+}
+
+#endif
+
---
+++ test/test_integration.cxx
@@ -0,0 +1,114 @@
+#include <catch.hpp>
+#include <catch_capprox.hpp>
+
+#include "helperFunctions.h"
+
+#include "BreitWigner.h"
+#include "DataPartition.h"
+#include "DataPoint.h"
+#include "DataSet.h"
+#include "FinalStateParticle.h"
+#include "HelicityFormalism.h"
+#include "ImportanceSampler.h"
+#include "logging.h"
+#include "make_unique.h"
+#include "MassAxes.h"
+#include "Model.h"
+#include "ModelIntegral.h"
+#include "Parameter.h"
+#include "ParticleFactory.h"
+#include "PDL.h"
+#include "PHSP.h"
+#include "Resonance.h"
+
+#include <future>
+#include <memory>
+
+/**
+ *  Test the integration
+ */
+
+namespace yap {
+
+//-------------------------
+// hidden helper function,
+// resolves C++ problem related to naming of functions and call to std::async below
+    const double sum_of_intensities(const Model& M, DataPartition& D, double ped)
+{
+    // calculate components
+    M.calculate(D);
+
+    // sum log of intensities over data points in partition
+    // if pedestal is zero
+    if (ped == 0)
+        return std::accumulate(D.begin(), D.end(), 0., [&](double & l, const DataPoint & d) {return l += log(intensity(M.initialStateParticles(), d));});
+    // else
+    return std::accumulate(D.begin(), D.end(), 0., [&](double & l, const DataPoint & d) {return l += (log(intensity(M.initialStateParticles(), d)) - ped);});
+}
+
+//-------------------------
+const double sum_of_intensity(const Model& M, DataPartition& D, double ped)
+{
+    if (M.initialStateParticles().empty())
+        throw exceptions::Exception("Model has no initialStateParticles", "sum_of_intensity");
+
+    return sum_of_intensities(M, D, ped);
+}
+
+//-------------------------
+const double sum_of_intensity(const Model& M, DataPartitionVector& DP, double ped)
+{
+    // if DataPartitionVector is empty
+    if (DP.empty())
+        throw exceptions::Exception("DataPartitionVector is empty", "sum_of_intensity");
+
+    // check no partitions are nullptr
+    if (std::any_of(DP.begin(), DP.end(), std::logical_not<DataPartitionVector::value_type>()))
+        throw exceptions::Exception("DataPartitionVector contains nullptr", "sum_of_intensity");
+
+    // if threading is unnecessary
+    if (DP.size() == 1)
+        return sum_of_intensity(M, *DP[0], ped);
+
+    if (M.initialStateParticles().empty())
+        throw exceptions::Exception("Model has no InitialStateParticles", "sum_of_intensity");
+
+    std::vector<std::future<double> > partial_sums;
+    partial_sums.reserve(DP.size());
+
+    // create thread for calculation on each partition
+    for (auto& P : DP)
+        // since std::async copies its arguments, even if they are supposed to be references, we need to use std::ref and std::cref
+        partial_sums.push_back(std::async(std::launch::async, sum_of_intensities, std::cref(M), std::ref(*P), ped));
+
+    // wait for each partition to finish calculating
+    return std::accumulate(partial_sums.begin(), partial_sums.end(), 0.,
+                           [](double & l, std::future<double>& s) {return l += s.get();});
+}
+
+}
+
+TEST_CASE("integration")
+{
+    // disable debug logs in test
+    //yap::disableLogs(el::Level::Debug);
+    yap::plainLogs(el::Level::Debug);
+
+    const unsigned nPoints = 1000;
+
+    auto M = create_model();
+    auto data = generate_data(M, nPoints);
+    auto partitions = yap::DataPartitionBlock::create(data, 4);
+
+    double bruteForceIntegral = yap::sum_of_intensity(*M, partitions, 0) / nPoints;
+    DEBUG("bruteForceIntegral = " << bruteForceIntegral);
+
+    yap::ModelIntegral integral(*M);
+
+    yap::ImportanceSampler::calculate(integral, partitions);
+    double smartIntegral = integral.integral().value;
+    DEBUG("smartIntegral = " << smartIntegral);
+
+    REQUIRE(bruteForceIntegral == Approx(smartIntegral));
+}
+
