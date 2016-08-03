3549a4ef4e49be41c5fd5f9a95c7aa27b43ef695
Johannes Rauch <j.rauch@tum.de>
2016-08-03 18:15:13 +0200

    can now also transfor covariance

--- include/AmplitudeBasis.h
+++ include/AmplitudeBasis.h
@@ -24,6 +24,8 @@
 #include "fwd/AmplitudeBasis.h"
 
 #include "Exceptions.h"
+#include "Matrix.h"
+#include "Vector.h"
 
 #include <complex>
 
@@ -31,125 +33,238 @@ namespace yap {
 
 namespace basis {
 
+/// \class basis
+/// base class for amplitude bases
+template <typename T>
+class basis {
+
+public:
+    /// constructor
+    /// \param c1 1st amplitude
+    /// \param c2 2nd amplitude
+    /// \param c3 3rd amplitude
+    /// \param cov 3x3 covariance matrix
+    explicit basis(std::complex<T> c1, std::complex<T> c2, std::complex<T> c3, SquareMatrix<T, 3> cov = zeroMatrix<T, 3, 3>()) :
+        coordinates_({c1, c2, c3}), covariance_(cov)
+    {}
+
+    /// constructor
+    /// \param c vector of amplitudes
+    /// \param cov 3x3 covariance matrix
+    explicit basis(Vector<std::complex<T>, 3> c, SquareMatrix<T, 3> cov = SquareMatrix<T, 3>::zeroMatrix()) :
+        coordinates_(c), covariance_(cov)
+    {}
+
+    /// constructor
+    /// \param cov 3x3 covariance matrix
+    explicit basis(SquareMatrix<T, 3> cov) :
+        coordinates_({0, 0, 0}), covariance_(cov)
+    {}
+
+    /// \return coordinates
+    const Vector<std::complex<T>, 3>& coordinates() const
+    { return coordinates_; }
+
+    /// \return covariance matrix
+    const SquareMatrix<T, 3>& covariance() const
+    { return covariance_; }
+
+protected:
+    // conversion constructor
+    explicit basis(const basis<T>& other, const SquareMatrix<T, 3>& jacobian) :
+        coordinates_(jacobian * other.coordinates_),
+        covariance_(jacobian * other.covariance_ * jacobian.transpose())
+    {}
+
+private:
+    /// vector of coordinates
+    Vector<std::complex<T>, 3> coordinates_;
+    /// covariance matrix
+    SquareMatrix<T, 3> covariance_;
+};
+
 /// \struct canonical
-/// stores amplitudes in canonical basis
+/// stores amplitudes in canonical basis (S, P, D)
 /// \tparam T type stored in amplitudes
 /// \defgroup AmplitudeBasis struct for converting among amplitude bases
 template <typename T>
-struct canonical {
-    /// S amplitude (l=0)
-    std::complex<T> S;
-    /// P amplitude (l=1)
-    std::complex<T> P;
-    /// D amplitude (l=2)
-    std::complex<T> D;
+class canonical : public basis<T> {
 
-    /// constructor
-    /// \param s S amplitude
-    /// \param p P amplitude
-    /// \param d D amplitude
-    explicit constexpr canonical(std::complex<T> s, std::complex<T> p, std::complex<T> d) :
-        S(s), P(p), D(d) {};
+public:
+    /// forward all base class constructors
+    // order of the amplitudes is: S, P, D
+    template<typename... Args>
+    canonical(Args&&... args) :
+        basis<T>(std::forward<Args>(args)...)
+    {}
 
     /// casting constructor
     explicit constexpr canonical(const transversity<T>& t) :
-        S(sqrt(2. / 3.) * t.parallel - sqrt(1. / 3.) * t.longitudinal),
-        P(t.perpendicular),
-        D(sqrt(1. / 3.) * t.parallel + sqrt(2. / 3.) * t.longitudinal)
+        basis<T>(t, t_to_c)
     {}
 
     /// casting constructor
     explicit constexpr canonical(const helicity<T>& h) :
-        S(sqrt(1. / 3.) * (h.plus + h.minus - h.zero)),
-        P(sqrt(1. / 2.) * (h.plus - h.minus)),
-        D(sqrt(1. / 6.) * (h.plus + h.minus) + sqrt(2. / 3.) * h.zero)
+        basis<T>(h, h_to_c)
     {}
 
+    /// \return S amplitude (l=0)
+    const std::complex<T> s() const
+    { return basis<T>::coordinates()[0]; }
+
+    /// \return P amplitude (l=1)
+    const std::complex<T> p() const
+    { return basis<T>::coordinates()[1]; }
+
+    /// \return D amplitude (l=2)
+    const std::complex<T> d() const
+    { return basis<T>::coordinates()[2]; }
+
     /// access canonical amplitude via angular momentum
-    /// \param L angular momentum to retrieve amplitude for
-    const std::complex<T>& operator[] (size_t L)
+    /// \param l angular momentum to retrieve amplitude for
+    const std::complex<T> operator[] (size_t l)
     {
-        switch (L) {
+        switch (l) {
             case 0:
-                return S;
+                return s();
             case 1:
-                return P;
+                return p();
             case 2:
-                return D;
+                return d();
             default :
-                throw exceptions::Exception("L must be 0, 1, or 2", "canonical::operator[]");
+                throw exceptions::Exception("l must be 0, 1, or 2", "canonical::operator[]");
         }
     }
+
+private:
+    // transformation matrix (jacobian) from transversity to canonical
+    static const SquareMatrix<T, 3> t_to_c;
+
+    // transformation matrix (jacobian) from helicity to canonical
+    static const SquareMatrix<T, 3> h_to_c;
 };
 
-/// \struct transversity
-/// Stores amplitudes in transversity basis
+/// \class transversity
+/// Stores amplitudes in transversity basis (longitudinal, parallel, perpendicular)
 /// \ingroup AmplitudeBasis
 template <typename T>
-struct transversity {
-    /// longitudinal amplitude
-    std::complex<T> longitudinal;
-    /// parallel amplitude
-    std::complex<T> parallel;
-    /// perpendicular amplitude
-    std::complex<T> perpendicular;
+class transversity : public basis<T> {
 
-    /// constructor
-    /// \param l    longitudinal amplitude
-    /// \param par  parallel amplitude
-    /// \param perp perpendicular amplitude
-    explicit constexpr transversity(std::complex<T> l, std::complex<T> par, std::complex<T> perp) :
-        longitudinal(l), parallel(par), perpendicular(perp) {};
+public:
+    /// forward all base class constructors
+    // order of the amplitudes is: longitudinal, parallel, perpendicular
+    template<typename... Args>
+    transversity(Args&&... args) :
+        basis<T>(std::forward<Args>(args)...)
+    {}
 
     /// casting constructor
     explicit constexpr transversity(const canonical<T>& c) :
-        longitudinal (-sqrt(1. / 3.) * c.S + sqrt(2. / 3.) * c.D),
-        parallel     ( sqrt(2. / 3.) * c.S + sqrt(1. / 3.) * c.D),
-        perpendicular( c.P)
+        basis<T>(c, c_to_t)
     {}
 
     /// casting constructor
     explicit constexpr transversity(const helicity<T>& h) :
-        longitudinal (h.zero),
-        parallel     (sqrt(0.5) * (h.plus + h.minus)),
-        perpendicular(sqrt(0.5) * (h.plus - h.minus))
+        basis<T>(h, h_to_t)
     {}
+
+    /// \return longitudinal amplitude
+    const std::complex<T> longitudinal() const
+    { return basis<T>::coordinates()[0]; }
+
+    /// \return parallel amplitude
+    const std::complex<T> parallel() const
+    { return basis<T>::coordinates()[1]; }
+
+    /// \return perpendicular amplitude
+    const std::complex<T> perpendicular() const
+    { return basis<T>::coordinates()[2]; }
+
+private:
+    // transformation matrix (jacobian) from canonical to transversity
+    static const SquareMatrix<T, 3> c_to_t;
+
+    // transformation matrix (jacobian) from helicity to transversity
+    static const SquareMatrix<T, 3> h_to_t;
 };
 
-/// \struct helicity
-/// Stores amplitudes in helicity basis
+
+/// \class helicity
+/// Stores amplitudes in helicity basis (zero, plus, minus)
 /// \ingroup AmplitudeBasis
 template <typename T>
-struct helicity {
-    /// zero amplitude A_0
-    std::complex<T> zero;
-    /// plus amplitude A_+
-    std::complex<T> plus;
-    /// minus amplitude A_-
-    std::complex<T> minus;
+class helicity : public basis<T> {
 
-    /// constructor
-    /// \param z zero amplitude A_0
-    /// \param p plus amplitude A_+
-    /// \param m minus amplitude A_-
-    explicit constexpr helicity(std::complex<T> z, std::complex<T> p, std::complex<T> m) :
-        zero(z), plus(p), minus(m) {};
+public:
+    /// forward all base class constructors
+    // order of the amplitudes is: zero, plus, minus
+    template<typename... Args>
+    helicity(Args&&... args) :
+        basis<T>(std::forward<Args>(args)...)
+    {}
 
     /// casting constructor
     explicit constexpr helicity(const canonical<T>& c) :
-        zero (-sqrt(1. / 3.) * c.S + sqrt(2. / 3.) * c.D),
-        plus ( sqrt(1. / 3.) * c.S + sqrt(1. / 2.) * c.P + sqrt(1. / 6.) * c.D),
-        minus( sqrt(1. / 3.) * c.S - sqrt(1. / 2.) * c.P + sqrt(1. / 6.) * c.D)
+        basis<T>(c, c_to_h)
     {}
 
     /// casting constructor
     explicit constexpr helicity(const transversity<T>& t) :
-        zero (t.longitudinal),
-        plus (sqrt(0.5) * (t.parallel + t.perpendicular)),
-        minus(sqrt(0.5) * (t.parallel - t.perpendicular))
+        basis<T>(t, t_to_h)
     {}
+
+    /// \return zero amplitude A_0
+    const std::complex<T> zero() const
+    { return basis<T>::coordinates()[0]; }
+
+    /// \return plus amplitude A_+
+    const std::complex<T> plus() const
+    { return basis<T>::coordinates()[1]; }
+
+    /// \return minus amplitude A_-
+    const std::complex<T> minus() const
+    { return basis<T>::coordinates()[2]; }
+
+private:
+    // transformation matrix (jacobian) from canonical to helicity
+    static const SquareMatrix<T, 3> c_to_h;
+
+    // transformation matrix (jacobian) from transversity to helicity
+    static const SquareMatrix<T, 3> t_to_h;
 };
 
+
+/// transformation matrix definitions
+
+template <typename T>                        //  longitudinal parallel     perpendicular
+const SquareMatrix<T, 3> canonical<T>::t_to_c({{-sqrt(1./3.), sqrt(2./3.), 0},     // S
+                                               { 0,           0,           1},     // P
+                                               { sqrt(2./3.), sqrt(1./3.), 0}});   // D
+
+template <typename T>                        //  zero         plus          minus
+const SquareMatrix<T, 3> canonical<T>::h_to_c({{-sqrt(1./3.), sqrt(1./3.),  sqrt(1./3.)},    // S
+                                               { 0,           sqrt(0.5),   -sqrt(0.5)  },    // P
+                                               { sqrt(2./3.), sqrt(1./6.),  sqrt(1./6.)}});  // D
+
+template <typename T>                           //  S              P  D
+const SquareMatrix<T, 3> transversity<T>::c_to_t({{-sqrt(1. / 3.), 0, sqrt(2. / 3.)},   // longitudinal
+                                                  { sqrt(2. / 3.), 0, sqrt(1. / 3.)},   // parallel
+                                                  { 0,             1, 0            }}); // perpendicular
+
+template <typename T>                           // zero plus        minus
+const SquareMatrix<T, 3> transversity<T>::h_to_t({{1,   0,          0        },    // longitudinal
+                                                  {0,   sqrt(0.5),  sqrt(0.5)},    // parallel
+                                                  {0,   sqrt(0.5), -sqrt(0.5)}});  // perpendicular
+
+template <typename T>                       //  S             P          D
+const SquareMatrix<T, 3> helicity<T>::c_to_h({{-sqrt(1./3.),  0,         sqrt(2./3.)},    // zero
+                                              { sqrt(1./3.),  sqrt(0.5), sqrt(1./6.)},    // plus
+                                              { sqrt(1./3.), -sqrt(0.5), sqrt(1./6.)}});  // minus
+
+template <typename T>                       // longitudinal parallel    perpendicular
+const SquareMatrix<T, 3> helicity<T>::t_to_h({{1,           0,          0        },    // zero
+                                              {0,           sqrt(0.5),  sqrt(0.5)},    // plus
+                                              {0,           sqrt(0.5), -sqrt(0.5)}});  // minus
 }
 
 }
--- test/test_BasisTransformations.cxx
+++ test/test_BasisTransformations.cxx
@@ -16,22 +16,22 @@ TEST_CASE( "BasisTransformations" )
     auto c1 = yap::basis::canonical<double>(t1);
     auto t2 = yap::basis::transversity<double>(c1);
 
-    REQUIRE( t1.longitudinal  == Catch::Detail::CApprox(t2.longitudinal) );
-    REQUIRE( t1.parallel      == Catch::Detail::CApprox(t2.parallel) );
-    REQUIRE( t1.perpendicular == Catch::Detail::CApprox(t2.perpendicular) );
+    REQUIRE( t1.longitudinal()  == Catch::Detail::CApprox(t2.longitudinal()) );
+    REQUIRE( t1.parallel()      == Catch::Detail::CApprox(t2.parallel()) );
+    REQUIRE( t1.perpendicular() == Catch::Detail::CApprox(t2.perpendicular()) );
 
     auto h1 = yap::basis::helicity<double>(t1);
     auto h2 = yap::basis::helicity<double>(c1);
 
-    REQUIRE( h1.zero  == Catch::Detail::CApprox(h2.zero ) );
-    REQUIRE( h1.plus  == Catch::Detail::CApprox(h2.plus ) );
-    REQUIRE( h1.minus == Catch::Detail::CApprox(h2.minus) );
+    REQUIRE( h1.zero()  == Catch::Detail::CApprox(h2.zero() ) );
+    REQUIRE( h1.plus()  == Catch::Detail::CApprox(h2.plus() ) );
+    REQUIRE( h1.minus() == Catch::Detail::CApprox(h2.minus()) );
 
     auto t3 = yap::basis::transversity<double>(h1);
 
-    REQUIRE( t3.longitudinal  == Catch::Detail::CApprox(t1.longitudinal ) );
-    REQUIRE( t3.parallel      == Catch::Detail::CApprox(t1.parallel     ) );
-    REQUIRE( t3.perpendicular == Catch::Detail::CApprox(t1.perpendicular) );
+    REQUIRE( t3.longitudinal()  == Catch::Detail::CApprox(t1.longitudinal() ) );
+    REQUIRE( t3.parallel()      == Catch::Detail::CApprox(t1.parallel()     ) );
+    REQUIRE( t3.perpendicular() == Catch::Detail::CApprox(t1.perpendicular()) );
 
     auto c2 = yap::basis::canonical<double>(h1);
 
