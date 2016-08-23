ba74dcde5815b104518194f04834d4b358fa7924
pdigiglio <p.digiglio91@gmail.com>
2016-08-23 19:10:38 +0200

    [InvariantMassBinning] first version of InvariantMassBinning

---
+++ include/InvariantMassBinning.h
@@ -0,0 +1,54 @@
+/*  YAP - Yet another PWA toolkit
+    Copyright 2015, Technische Universitaet Muenchen,
+    Authors: Daniel Greenwald, Johannes Rauch
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+/// \file
+
+
+#ifndef  yap_InvariantMassBinning_h
+#define  yap_InvariantMassBinning_h
+
+#include "fwd/CachedValue.h"
+
+#include "StaticDataAccessor.h"
+
+namespace yap {
+
+/// \class InvariantMassBinning
+/// \author Paolo Di Giglio
+class InvariantMassBinning : public StaticDataAccessor
+{
+public:
+	/// Constructor.
+	/// The #ParticleCombinationEqualTo is defaulted to #equal_by_orderless_content.
+	explicit InvariantMassBinning(Model& m, const std::vector<double>& bins);
+	virtual void calculate(DataPoint& d, StatusManager& sm) const override;
+private:
+	/// Partitioning of the mass axis.
+	/// It contains the \f$n\f$ lower bounds of the bins plus
+	/// the upper bound of the last bin:
+	/// \f[
+	/// [m_0,m_1) \cup [m_1,m_2) \dots [m_{n-1}, m_n).
+	/// \f]
+	const std::vector<double> Bins_;
+
+	std::shared_ptr<RealCachedValue> BinNumber_;
+};
+
+}
+
+#endif
---
+++ src/InvariantMassBinning.cxx
@@ -0,0 +1,68 @@
+#include "InvariantMassBinning.h"
+
+#include "CachedValue.h"
+#include "CalculationStatus.h"
+#include "Exceptions.h"
+#include "FourMomenta.h"
+#include "Model.h"
+#include "ParticleCombination.h"
+#include "StatusManager.h"
+
+#include <algorithm>
+#include <functional>
+
+namespace yap {
+
+//-------------------------
+InvariantMassBinning::InvariantMassBinning(Model& m, const std::vector<double>& bins) :
+	StaticDataAccessor(m, equal_by_orderless_content),
+	Bins_(bins),
+	BinNumber_(RealCachedValue::create(*this))
+{
+	if (Bins_.size() < 2)
+		throw exceptions::Exception("At least two bins must be specified",
+                                    "InvariantMassBinning::InvariantMassBinning");
+
+	// use std::less_equal so that if two elements are the same, std::is_sorted
+	// will return false
+	if (!std::is_sorted(Bins_.cbegin(), Bins_.cend(), std::less_equal<decltype(*Bins_.cbegin())>()))
+		throw exceptions::Exception("Elements of the vector are not monotonically increasing",
+                                    "InvariantMassBinning::InvariantMassBinning");
+}
+
+//-------------------------
+void InvariantMassBinning::calculate(DataPoint& d, StatusManager& sm) const
+{
+	// set all bins to uncalculated
+	sm.set(*BinNumber_, CalculationStatus::uncalculated);
+
+	// loop over particle combinations -> indices
+	for (auto& pc_i : symmetrizationIndices()) {
+		// check if calculation is necessary
+		if (sm.status(*BinNumber_, pc_i.second ) == CalculationStatus::uncalculated) {
+			auto invariant_mass = model()->fourMomenta()->m(d, pc_i.first);
+
+			// get the first lower bound that is greater than the invariant mass
+			auto bin = std::upper_bound(Bins_.cbegin(), Bins_.cend(), invariant_mass);
+
+			// check if the value above the upper limit
+			if (bin == Bins_.cend())
+				throw exceptions::Exception("Mass is above the upper bin",
+						"InvariantMassBinning::calculate");
+			// check if the value below the lower limit
+			if (bin == Bins_.cbegin())
+				throw exceptions::Exception("Mass is below the lower bin",
+						"InvariantMassBinning::calculate");
+
+			// if the previous checks passed, set bin to the actual value
+			// of the containing bin (i.e. bin is the last value in the vector
+			// that is not greater than invariant_mass);
+			--bin;
+
+			// set the value into the DataPoint
+			BinNumber_->setValue(std::distance(Bins_.cbegin(), bin), d, pc_i.second, sm);
+		}
+	}
+}
+
+}
