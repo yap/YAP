c222218db5e2bea6d7eb4cda2be29fb50df21770
pdigiglio <p.digiglio91@gmail.com>
2016-09-14 11:37:26 +0200

    [travis-cfg] remove install scripts, set notifications

--- travisCI/install-root.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# Download ROOT archive
-wget https://root.cern.ch/download/root_v6.06.08.Linux-ubuntu14-x86_64-gcc4.8.tar.gz -O root.tar.gz
-
-# Extract ROOT archive in this directory
-tar xzf root.tar.gz 
-
-# !! Remember to `. ./root/bin/thisroot.sh` in the .travis.yml file !!
