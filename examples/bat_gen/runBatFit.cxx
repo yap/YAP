accf2ad05c23a62779e08b58149e22e23c059f5a
Johannes Rauch <j.rauch@tum.de>
2016-09-23 15:01:52 +0200

    easier switching between models and restore default values

--- examples/bat_gen/runBatFit.cxx
+++ examples/bat_gen/runBatFit.cxx
@@ -37,9 +37,9 @@ int main()
     yap::plainLogs(el::Level::Info);
 
     // open file
-    // std::string model_name = "D3PI";
-    //std::string model_name = "DKKPI";
-    std::string model_name = "D4PI";
+    std::string model_name = "D3PI";
+    // std::string model_name = "DKKPI";
+    // std::string model_name = "D4PI";
 
     auto file = TFile::Open(("output/" + model_name + "_mcmc.root").data(), "READ");
     if (file->IsZombie())
@@ -56,44 +56,56 @@ int main()
         throw yap::exceptions::Exception("could not retrieve mcmc tree", "main");
 
     // create model
-    // auto m = dkkpi_fit(model_name + "_fit", yap_model<yap::HelicityFormalism>(), find_mass_axes(*t_pars));
-    // auto m = d3pi_fit(model_name + "_fit", yap_model<yap::ZemachFormalism>(), find_mass_axes(*t_pars));
-    auto m = d4pi_fit(model_name + "_fit");
+    bat_fit* m;
+    double D_mass(0);
 
-    //double D_mass = 1.86961;
-    double D_mass = 1.8648400;
+    if (model_name == "D3PI") {
+        m = new bat_fit(d3pi_fit(model_name + "_fit", yap_model<yap::ZemachFormalism>(), find_mass_axes(*t_pars)));
+        D_mass = 1.86961; // D+
+    }
+    else if (model_name == "DKKPI") {
+        m = new bat_fit(dkkpi_fit(model_name + "_fit", yap_model<yap::HelicityFormalism>(), find_mass_axes(*t_pars)));
+        D_mass = 1.86961; // D+
+    }
+    else if (model_name == "D4PI") {
+        m = new bat_fit(d4pi_fit(model_name + "_fit"));
+        D_mass = 1.8648400; // D0
+    }
+    else
+        LOG(ERROR) << "No model loaded";
 
     // load fit data and partition it
-    load_data(m.fitData(), *m.model(), m.axes(), D_mass, *t_mcmc, 5e5);
-    m.fitPartitions() = yap::DataPartitionBlock::create(m.fitData(), 6);
+    LOG(INFO) << "Load data";
+    load_data(m->fitData(), *m->model(), m->axes(), D_mass, *t_mcmc, 1e4);
+    m->fitPartitions() = yap::DataPartitionBlock::create(m->fitData(), 4);
 
     // get FSP mass ranges
-    auto m2r = yap::squared(mass_range(D_mass, m.axes(), m.model()->finalStateParticles()));
+    auto m2r = yap::squared(mass_range(D_mass, m->axes(), m->model()->finalStateParticles()));
 
     // generate integration data
     std::mt19937 g(0);
-    unsigned n_integrationPoints = 1e6;
+    unsigned n_integrationPoints = 2e4;
     if (true) {
-        m.integrationPointGenerator() = std::bind(yap::phsp<std::mt19937>, std::cref(*m.model()), D_mass, m.axes(), m2r, g, std::numeric_limits<unsigned>::max());
-        // m.setNIntegrationPoints(4e4, 4e4);
-        m.setNIntegrationPoints(n_integrationPoints, 1e5, 4);
+        m->integrationPointGenerator() = std::bind(yap::phsp<std::mt19937>, std::cref(*m->model()), D_mass, m->axes(), m2r, g, std::numeric_limits<unsigned>::max());
+        // m->setNIntegrationPoints(4e4, 4e4);
+        m->setNIntegrationPoints(n_integrationPoints, 1e4, 4);
         LOG(INFO) << "Generating integration points on the fly";
     } else {
         // get FSP mass ranges
-        auto m2r = yap::squared(mass_range(D_mass, m.axes(), m.model()->finalStateParticles()));
+        auto m2r = yap::squared(mass_range(D_mass, m->axes(), m->model()->finalStateParticles()));
         
         // generate integration data
-        std::generate_n(std::back_inserter(m.integralData()), n_integrationPoints,
-                        std::bind(yap::phsp<std::mt19937>, std::cref(*m.model()), D_mass, m.axes(), m2r, g, std::numeric_limits<unsigned>::max()));
-        m.integralPartitions() = yap::DataPartitionBlock::create(m.integralData(), 4);
-        LOG(INFO) << "Created " << m.integralData().size() << " data points (" << (m.integralData().bytes() * 1.e-6) << " MB)";
+        std::generate_n(std::back_inserter(m->integralData()), n_integrationPoints,
+                        std::bind(yap::phsp<std::mt19937>, std::cref(*m->model()), D_mass, m->axes(), m2r, g, std::numeric_limits<unsigned>::max()));
+        m->integralPartitions() = yap::DataPartitionBlock::create(m->integralData(), 4);
+        LOG(INFO) << "Created " << m->integralData().size() << " data points (" << (m->integralData().bytes() * 1.e-6) << " MB)";
     }
 
-    // TH2D* h2_fit_data = hist2(*m.model()->fourMomenta(), m.axes(), m2r, m.fitData());
-    // TH2D* h2_int_data = hist2(*m.model()->fourMomenta(), m.axes(), m2r, m.integralData());
+    // TH2D* h2_fit_data = hist2(*m->model()->fourMomenta(), m->axes(), m2r, m->fitData());
+    // TH2D* h2_int_data = hist2(*m->model()->fourMomenta(), m->axes(), m2r, m->integralData());
 
-    // TH1D* h1_fit_data = hist1(*m.model()->fourMomenta(), m.axes()[0], m2r[0], m.fitData());
-    // TH1D* h1_int_data = hist1(*m.model()->fourMomenta(), m.axes()[0], m2r[0], m.integralData());
+    // TH1D* h1_fit_data = hist1(*m->model()->fourMomenta(), m->axes()[0], m2r[0], m->fitData());
+    // TH1D* h1_int_data = hist1(*m->model()->fourMomenta(), m->axes()[0], m2r[0], m->integralData());
 
     // TCanvas* C = new TCanvas("C","C");
     // C->Divide(1,2);
@@ -106,44 +118,44 @@ int main()
     // return 0;
 
     // open log file
-    BCLog::OpenLog("output/" + m.GetSafeName() + "_log.txt", BCLog::detail, BCLog::detail);
+    BCLog::OpenLog("output/" + m->GetSafeName() + "_log.txt", BCLog::detail, BCLog::detail);
 
     // set precision
-    m.SetPrecision(BCEngineMCMC::kMedium);
-    m.SetNIterationsPreRunMax(1e6);
-    m.SetNChains(4);
-    // m.SetMinimumEfficiency(0.85);
-    // m.SetMaximumEfficiency(0.99);
+    m->SetPrecision(BCEngineMCMC::kMedium);
+    m->SetNIterationsPreRunMax(1e6);
+    m->SetNChains(4);
+    // m->SetMinimumEfficiency(0.85);
+    // m->SetMaximumEfficiency(0.99);
 
-    m.SetNIterationsRun(static_cast<int>(50e3 / m.GetNChains()));
+    m->SetNIterationsRun(static_cast<int>(50e3 / m->GetNChains()));
 
-    // m.WriteMarkovChain("output/" + m.GetSafeName() + "_mcmc.root", "RECREATE", true, false);
+    // m->WriteMarkovChain("output/" + m->GetSafeName() + "_mcmc.root", "RECREATE", true, false);
 
     // start timing:
     auto start = std::chrono::steady_clock::now();
 
     // run MCMC, marginalizing posterior
-    m.MarginalizeAll(BCIntegrate::kMargMetropolis);
+    m->MarginalizeAll(BCIntegrate::kMargMetropolis);
 
     // end timing
     auto end = std::chrono::steady_clock::now();
 
-    m.FindMode(m.GetBestFitParameters());
+    m->FindMode(m->GetBestFitParameters());
 
-    m.PrintSummary();
-    m.PrintAllMarginalized("output/" + m.GetSafeName() + "_plots.pdf", 2, 2);
+    m->PrintSummary();
+    m->PrintAllMarginalized("output/" + m->GetSafeName() + "_plots.pdf", 2, 2);
 
-    // m.SetNIterationsRun(static_cast<int>(10e3 / m.GetNChains()));
-    // // m.SetKnowledgeUpdateDrawingStyle(BCAux::kKnowledgeUpdateDetailedPosterior);
-    // m.PrintKnowledgeUpdatePlots("output/" + m.GetSafeName() + "_update.pdf", 2, 2, true);
+    // m->SetNIterationsRun(static_cast<int>(10e3 / m->GetNChains()));
+    // // m->SetKnowledgeUpdateDrawingStyle(BCAux::kKnowledgeUpdateDetailedPosterior);
+    // m->PrintKnowledgeUpdatePlots("output/" + m->GetSafeName() + "_update.pdf", 2, 2, true);
 
     // timing:
     auto diff = end - start;
     auto ms = std::chrono::duration<double, std::micro>(diff).count();
-    auto nevents = (m.GetNIterationsPreRun() + m.GetNIterationsRun()) * m.GetNChains();
-    BCLog::OutSummary(std::string("Seconds = ") + std::to_string(ms / 1.e6) + " for " + std::to_string(nevents) + " iterations, " + std::to_string(m.likelihoodCalls()) + " calls");
+    auto nevents = (m->GetNIterationsPreRun() + m->GetNIterationsRun()) * m->GetNChains();
+    BCLog::OutSummary(std::string("Seconds = ") + std::to_string(ms / 1.e6) + " for " + std::to_string(nevents) + " iterations, " + std::to_string(m->likelihoodCalls()) + " calls");
     BCLog::OutSummary(std::to_string(ms / nevents) + " microsec / iteration");
-    BCLog::OutSummary(std::to_string(ms / m.likelihoodCalls()) + " microsec / call");
+    BCLog::OutSummary(std::to_string(ms / m->likelihoodCalls()) + " microsec / call");
 
     // // close log file
     BCLog::OutSummary("Exiting");
--- examples/bat_gen/runBatFitFitFractions.cxx
+++ examples/bat_gen/runBatFitFitFractions.cxx
@@ -7,6 +7,7 @@
 
 #include "fit_fitFraction.h"
 #include "models/d3pi.h"
+#include "models/d4pi.h"
 #include "tools.h"
 
 #include <DecayTree.h>
@@ -36,8 +37,8 @@ int main()
     // create bat_fit object
     auto m = d3pi_fit_fitFractions("D3PI_frac_fit", yap_model<yap::ZemachFormalism>());
     double D_mass = 1.86961;
-    /*auto m = d4pi_fit_fitFraction();
-    double D_mass = 1.8648400;*/
+    //auto m = d4pi_fit_fitFraction();
+    //double D_mass = 1.8648400;
 
     FLOG(INFO) << "Created model";
 
