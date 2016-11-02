0b753ffdde18c73b431a515238be3fca3eac3eb7
Johannes Rauch <j.rauch@tum.de>
2016-11-02 13:03:54 +0100

    rebase and rename

--- include/DataPoint.h
+++ include/DataPoint.h
@@ -28,13 +28,6 @@
 
 namespace yap {
 
-/// precompiler flag for data storage type
-#ifdef DATA_POINT_FLOAT
-    using data_point_type = float;
-# else
-    using data_point_type = double;
-#endif
-
 /// \class DataPoint
 /// \brief Class for holding data and cached values per data point for fast calculation
 /// \author Johannes Rauch, Daniel Greenwald
@@ -66,11 +59,18 @@ public:
 
 private:
 
+/// precompiler flag for data storage type
+#ifdef YAP_DATA_POINT_FLOAT
+    using type = float;
+# else
+    using type = double;
+#endif
+
     /// Data storage for all DataAccessors
     /// first index is for the DataAccessor
     /// second index is for the symmeterization state (as known by the DataAccessor) internal to the DataAccessor
     /// i = symIndex * size + position
-    std::vector<std::vector<data_point_type> > Data_;
+    std::vector<std::vector<type> > Data_;
 
 };
 
