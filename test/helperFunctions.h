5a2caad9ee01ad9fcea2d5a2b80842d4d94c5caf
Daniel Greenwald <daniel.greenwald@tum.de>
2017-02-06 16:45:23 +0900

    [test/helperFunctions] locking models (bugfix)

--- test/helperFunctions.h
+++ test/helperFunctions.h
@@ -56,6 +56,8 @@ inline std::shared_ptr<yap::Model> d4pi()
     D->addWeakDecay(rho, rho);
     D->addWeakDecay(a_1, piMinus);
 
+    M->lock();
+    
     return M;
 }
 
@@ -86,6 +88,8 @@ inline std::shared_ptr<yap::Model> dkkp(int pdg_D, std::vector<int> fsps)
         D->addWeakDecay(res, kPlus);
     }
 
+    M->lock();
+    
     *free_amplitude(*M, yap::from(*D), yap::l_equals(0)) = 0.5;
     *free_amplitude(*M, yap::from(*D), yap::l_equals(1)) = 1.;
     *free_amplitude(*M, yap::from(*D), yap::l_equals(2)) = 30.;
@@ -121,6 +125,8 @@ std::shared_ptr<yap::Model> d3pi()
     // Add channels to D
     D->addWeakDecay(rho, piPlus);
 
+    M->lock();
+    
     return M;
 }
 
